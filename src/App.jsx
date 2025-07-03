7dd1f33ebc09ce933bb84b694ee4e89232c95b2d
Lin <lincarbajales@gmail.com>
2025-07-03 12:58:00 +0200

    Funcionalidad de pausar y reiniciar, falta reanudar

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,36 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [state, changeState] = useState(true);
+
   const tiempo = useRef(null);
   
-  useEffect(() => {
+  const arrancarCronometro = () => {
+    
   tiempo.current = setInterval(() => {
     setCount(count => count + 1);
-  }, 1000);
+    }, 1000);
   
-  return () => clearInterval(tiempo.current);
-  }, []);
+    return () => clearInterval(tiempo.current);
+  }
+
+  useEffect(() => arrancarCronometro() , []);
 
   const pausar = () => {
     clearInterval(tiempo.current);
+    changeState(false);
   };
+
+  const reiniciar = () => {
+    setCount(0);
+  }
 
   return (
     <>
       <h1>Cronómetro</h1>
       <p>Segundos: {count}</p>
-      <button onClick={pausar}>Pausar</button>
+      <button onClick={pausar}>{state ? 'Pausar' : 'Reanudar'}</button>
+      <button onClick={reiniciar}>Reiniciar</button>
     </>
   )
 }
